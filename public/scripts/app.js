528a8ec9cdfa07ee34386d5e304157556d531b73
Mokhtar Mohammadi <m.mohammadi1397s@gmail.com>
2025-06-03 19:24:15 +0300

    Compalted: Header

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,8 @@
 const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click", () => {
+const shoppingCartIcon = document.querySelector("#shopping-cart");
+const shoppingCartContent = document.querySelector("#shopping-cart-content");
+
+const themeHandler = () => {
   if (localStorage.theme === "dark") {
     document.documentElement.classList.remove("dark");
     localStorage.theme = "light";
@@ -7,4 +10,15 @@ toggleThemeBtn.addEventListener("click", () => {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
   }
-});
+};
+
+//* بجای این تابع فعلا از هاور استفاده می‌کنیم
+const shoppingCartHandler = () => {
+  const cls = ["opacity-0", "invisible", "opacity-100", "visible"];
+
+  cls.forEach((className) => {
+    shoppingCartContent.classList.toggle(className);
+  });
+};
+
+toggleThemeBtn.addEventListener("click", themeHandler);
