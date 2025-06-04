3f83a18b61a72c1ee0fd8dff2868388262ff4314
Mokhtar Mohammadi <m.mohammadi1397s@gmail.com>
2025-06-05 02:21:04 +0300

    Complated: Mobile Cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,16 @@
 const toggleThemeBtn = document.querySelector("#toggle-theme");
+const mobileThemeBtn = document.querySelector(".mobile-theme");
 const shoppingCartIcon = document.querySelector("#shopping-cart");
 const shoppingCartContent = document.querySelector("#shopping-cart-content");
 const mobileMenuBtn = document.querySelector("#mobile-menu-toggle");
+const mobileCartIcon = document.querySelector("#mobile-cart-toggle");
 const mobileMenuContent = document.querySelector("#mobile-menu-content");
-const overlay = document.querySelector(".overlay");
-const xMark = document.querySelector(".x-mark");
+const overlayElem = document.querySelector(".overlay");
+const closeMenuIcon = document.querySelector(".close-menu");
+const closeCartIcon = document.querySelector(".close-cart");
 const storeElement = document.querySelector("#store");
 const subMenuElem = document.querySelector(".sub-menu");
+const mobileCart = document.querySelector("#mobile-cart");
 
 const themeHandler = () => {
   if (localStorage.theme === "dark") {
@@ -18,19 +22,19 @@ const themeHandler = () => {
   }
 };
 
-//* بجای این تابع فعلا از هاور استفاده می‌کنیم
-const shoppingCartHandler = () => {
-  const cls = ["opacity-0", "invisible", "opacity-100", "visible"];
+// //* بجای این تابع فعلا از هاور استفاده می‌کنیم
+// const shoppingCartHandler = () => {
+//   const cls = ["opacity-0", "invisible", "opacity-100", "visible"];
 
-  cls.forEach((className) => {
-    shoppingCartContent.classList.toggle(className);
-  });
-};
+//   cls.forEach((className) => {
+//     shoppingCartContent.classList.toggle(className);
+//   });
+// };
 
 const openOrCloseMenuHandler = () => {
   mobileMenuContent.classList.toggle("-right-64");
   mobileMenuContent.classList.toggle("right-0");
-  overlay.classList.toggle("hidden");
+  overlayHandler();
 };
 
 const showSubMenu = () => {
@@ -38,7 +42,21 @@ const showSubMenu = () => {
   subMenuElem.classList.toggle("max-h-96");
 };
 
+const shoppingCartHandler = () => {
+  mobileCart.classList.toggle("-left-64");
+  mobileCart.classList.toggle("left-0");
+
+  overlayHandler();
+};
+
+const overlayHandler = () => {
+  overlayElem.classList.toggle("hidden");
+};
+
 toggleThemeBtn.addEventListener("click", themeHandler);
 mobileMenuBtn.addEventListener("click", openOrCloseMenuHandler);
-xMark.addEventListener("click", openOrCloseMenuHandler);
+closeMenuIcon.addEventListener("click", openOrCloseMenuHandler);
 storeElement.addEventListener("click", showSubMenu);
+mobileThemeBtn.addEventListener("click", themeHandler);
+mobileCartIcon.addEventListener("click", shoppingCartHandler);
+closeCartIcon.addEventListener("click", shoppingCartHandler);
