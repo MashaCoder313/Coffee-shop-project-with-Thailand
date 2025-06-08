ce1e7882e366626a6da7a3364a12e85b0c69c8e2
Mokhtar Mohammadi <m.mohammadi1397s@gmail.com>
2025-06-09 02:05:57 +0300

    Add : ...

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,18 @@
 const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click", () => {
+const mobileThemeBtn = document.querySelector(".mobile-theme");
+const shoppingCartIcon = document.querySelector("#shopping-cart");
+const shoppingCartContent = document.querySelector("#shopping-cart-content");
+const mobileMenuBtn = document.querySelector("#mobile-menu-toggle");
+const mobileCartIcon = document.querySelector("#mobile-cart-toggle");
+const mobileMenuContent = document.querySelector("#mobile-menu-content");
+const overlayElem = document.querySelector(".overlay");
+const closeMenuIcon = document.querySelector(".close-menu");
+const closeCartIcon = document.querySelector(".close-cart");
+const storeElement = document.querySelector("#store");
+const subMenuElem = document.querySelector(".sub-menu");
+const mobileCart = document.querySelector("#mobile-cart");
+
+const themeHandler = () => {
   if (localStorage.theme === "dark") {
     document.documentElement.classList.remove("dark");
     localStorage.theme = "light";
@@ -7,7 +20,46 @@ toggleThemeBtn.addEventListener("click", () => {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
   }
-});
+};
+
+// //* بجای این تابع فعلا از هاور استفاده می‌کنیم
+// const shoppingCartHandler = () => {
+//   const cls = ["opacity-0", "invisible", "opacity-100", "visible"];
+
+//   cls.forEach((className) => {
+//     shoppingCartContent.classList.toggle(className);
+//   });
+// };
+
+const openOrCloseMenuHandler = () => {
+  mobileMenuContent.classList.toggle("-right-64");
+  mobileMenuContent.classList.toggle("right-0");
+  overlayHandler();
+};
+
+const showSubMenu = () => {
+  subMenuElem.classList.toggle("max-h-0");
+  subMenuElem.classList.toggle("max-h-96");
+};
+
+const shoppingCartHandler = () => {
+  mobileCart.classList.toggle("-left-64");
+  mobileCart.classList.toggle("left-0");
+
+  overlayHandler();
+};
+
+const overlayHandler = () => {
+  overlayElem.classList.toggle("hidden");
+};
+
+toggleThemeBtn.addEventListener("click", themeHandler);
+mobileMenuBtn.addEventListener("click", openOrCloseMenuHandler);
+closeMenuIcon.addEventListener("click", openOrCloseMenuHandler);
+storeElement.addEventListener("click", showSubMenu);
+mobileThemeBtn.addEventListener("click", themeHandler);
+mobileCartIcon.addEventListener("click", shoppingCartHandler);
+closeCartIcon.addEventListener("click", shoppingCartHandler);
 
 //* BestSellingProducts
 
@@ -78,7 +130,7 @@ const bestSellingProducts = [
   },
 ];
 
-function renderBestSellers() {
+const renderBestSellers = () => {
   const wrapper = document.querySelector(".swiper-wrapper");
 
   bestSellingProducts.forEach((product) => {
@@ -133,7 +185,7 @@ function renderBestSellers() {
 
     wrapper.insertAdjacentHTML("beforeend", template);
   });
-}
+};
 
 // حتما بعد از لود صفحه اجرا شود
 document.addEventListener("DOMContentLoaded", renderBestSellers);
